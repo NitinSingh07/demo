dd5e587d3dccf7c7abc775efd636a866ef368c10
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2024-08-08 02:10:33 +0530

    feat: Add Event and User models

---
+++ Backend/models/event.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Event = mongoose.model("Event", EventSchema);
+export default Event;
